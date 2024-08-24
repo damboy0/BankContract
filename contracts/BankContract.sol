23462eae68a69077f4088f21c1c104583cce4989
damboy0 <fagbolade006@gmail.com>
2024-08-24 10:40:53 +0100

    updated

---
+++ contracts/BankContract.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.24;
+
+contract BankContract {
+
+    struct Account {
+        string name;
+        string email;
+        uint256 balance;
+    }
+
+    mapping(address => Account) public accounts;  // Mapping to store user accounts
+
+    event CreatedAccount(address indexed user, string name);
+    event Deposit(address indexed user, uint256 amount);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
+
+    function createAccount(string memory _name, string memory _email) public {
+        accounts[msg.sender] = Account({
+            name: _name,
+            email: _email,
+            balance: 0
+        });
+
+        emit CreatedAccount(msg.sender, _name);
+    }
+
+    function depositAccount() public payable {
+        require(msg.value > 0, "Deposit amount must be greater than zero");
+        
+        accounts[msg.sender].balance += msg.value; 
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function transferEther(address payable _to, uint256 _amount) public {
+        require(accounts[msg.sender].balance >= _amount, "Insufficient balance in sender account");
+        require(_amount > 0, "Transfer amount must be greater than zero");
+
+        accounts[msg.sender].balance -= _amount; 
+        (bool sent, ) = _to.call{value: _amount}("");
+        require(sent, "Ether not sent");
+
+        emit Transfer(msg.sender, _to, _amount);
+    }
+
+    function withdraw(uint256 _amount) public {
+        require(accounts[msg.sender].balance >= _amount, "Insufficient balance in account");
+        require(_amount > 0, "Withdraw amount must be greater than zero");
+
+        accounts[msg.sender].balance -= _amount;  
+        (bool sent, ) = msg.sender.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+
+        emit Withdraw(msg.sender, _amount);
+    }
+
+    // Function to check contract balance
+    function getContractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
