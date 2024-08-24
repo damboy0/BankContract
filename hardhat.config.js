72a28d07ab5dc7af8a41ed115651716138515a85
damboy0 <fagbolade006@gmail.com>
2024-08-24 12:07:01 +0100

    Updated config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_API_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
